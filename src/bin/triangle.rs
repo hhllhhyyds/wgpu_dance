63a649f17dd31fb65abf4c0a5c6110d15dc27fd5
hhllhhyyds <hhllhhyyds@sina.com>
2025-02-19 10:37:35 +0800

    present surface

--- src/bin/triangle.rs
+++ src/bin/triangle.rs
@@ -3,22 +3,161 @@ use std::{rc::Rc, sync::Arc};
 use parking_lot::Mutex;
 use winit::{
     application::ApplicationHandler,
-    event::{KeyEvent, WindowEvent},
+    dpi::{PhysicalPosition, PhysicalSize},
+    event::{
+        DeviceEvent, ElementState, KeyEvent, MouseButton, MouseScrollDelta, TouchPhase, WindowEvent,
+    },
     event_loop::{ActiveEventLoop, EventLoop},
     keyboard::{KeyCode, PhysicalKey},
     window::{Window, WindowId},
 };
 
 struct WgpuApp {
-    /// 避免窗口被释放
-    #[allow(unused)]
     window: Arc<Window>,
+    surface: wgpu::Surface<'static>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+    size_changed: bool,
 }
 
 impl WgpuApp {
     async fn new(window: Arc<Window>) -> Self {
-        // ...
-        Self { window }
+        let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::all(),
+            ..Default::default()
+        });
+        let surface = instance.create_surface(window.clone()).unwrap();
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await
+            .unwrap();
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    required_features: wgpu::Features::empty(),
+                    required_limits: if cfg!(target_arch = "wasm32") {
+                        wgpu::Limits::downlevel_webgl2_defaults()
+                    } else {
+                        wgpu::Limits::default()
+                    },
+                    label: None,
+                    memory_hints: wgpu::MemoryHints::Performance,
+                },
+                None,
+            )
+            .await
+            .unwrap();
+
+        let mut size = window.inner_size();
+        size.width = size.width.max(1);
+        size.height = size.height.max(1);
+
+        let caps = surface.get_capabilities(&adapter);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: caps.formats[0],
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo,
+            alpha_mode: caps.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+        surface.configure(&device, &config);
+
+        Self {
+            window,
+            surface,
+            device,
+            queue,
+            config,
+            size,
+            size_changed: false,
+        }
+    }
+
+    fn set_window_resized(&mut self, new_size: PhysicalSize<u32>) {
+        if new_size == self.size {
+            return;
+        }
+        self.size = new_size;
+        self.size_changed = true;
+    }
+
+    fn resize_surface_if_needed(&mut self) {
+        if self.size_changed {
+            self.config.width = self.size.width;
+            self.config.height = self.size.height;
+            self.surface.configure(&self.device, &self.config);
+            self.size_changed = false;
+        }
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let output = self.surface.get_current_texture()?;
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+
+        {
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                ..Default::default()
+            });
+        }
+
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
+
+    fn keyboard_input(&mut self, _event: &KeyEvent) -> bool {
+        false
+    }
+
+    fn mouse_click(&mut self, _state: ElementState, _button: MouseButton) -> bool {
+        false
+    }
+
+    fn mouse_wheel(&mut self, _delta: MouseScrollDelta, _phase: TouchPhase) -> bool {
+        false
+    }
+
+    fn cursor_move(&mut self, _position: PhysicalPosition<f64>) -> bool {
+        false
+    }
+
+    fn device_input(&mut self, _event: &DeviceEvent) -> bool {
+        false
+    }
+
+    fn update(&mut self) {
+        // remove `todo!()`
     }
 }
 
@@ -29,12 +168,11 @@ struct WgpuAppHandler {
 
 impl ApplicationHandler for WgpuAppHandler {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        // 恢复事件
         if self.app.as_ref().lock().is_some() {
             return;
         }
 
-        let window_attributes = Window::default_attributes().with_title("tutorial1-window");
+        let window_attributes = Window::default_attributes().with_title("triangle");
         let window = Arc::new(event_loop.create_window(window_attributes).unwrap());
 
         let wgpu_app = pollster::block_on(WgpuApp::new(window));
@@ -51,13 +189,23 @@ impl ApplicationHandler for WgpuAppHandler {
         _window_id: WindowId,
         window_event: WindowEvent,
     ) {
-        // 窗口事件
+        let mut app = self.app.lock();
+        assert!(app.as_ref().is_some());
+
+        let app = app.as_mut().unwrap();
+
         match window_event {
             WindowEvent::CloseRequested => {
                 event_loop.exit();
             }
-            WindowEvent::Resized(_size) => {
-                // 窗口大小改变
+            WindowEvent::Resized(physical_size) => {
+                if physical_size.width == 0 || physical_size.height == 0 {
+                    // 处理最小化窗口的事件
+                    log::info!("Window minimized!");
+                } else {
+                    log::info!("Window resized: {:?}", physical_size);
+                    app.set_window_resized(physical_size);
+                }
             }
             WindowEvent::KeyboardInput {
                 event: KeyEvent { physical_key, .. },
@@ -67,58 +215,26 @@ impl ApplicationHandler for WgpuAppHandler {
                 _ => {}
             },
             WindowEvent::RedrawRequested => {
-                // surface重绘事件
+                // surface 重绘事件
+                app.window.pre_present_notify();
+
+                match app.render() {
+                    Ok(_) => {}
+                    // 当展示平面的上下文丢失，就需重新配置
+                    Err(wgpu::SurfaceError::Lost) => eprintln!("Surface is lost"),
+                    // 所有其他错误（过期、超时等）应在下一帧解决
+                    Err(e) => eprintln!("{e:?}"),
+                }
+                // 除非我们手动请求，RedrawRequested 将只会触发一次。
+                app.window.request_redraw();
             }
             _ => (),
         }
     }
 }
 
-// 根据不同平台初始化日志。
-pub fn init_logger() {
-    cfg_if::cfg_if! {
-        if #[cfg(target_arch = "wasm32")] {
-            // 使用查询字符串来获取日志级别。
-            let query_string = web_sys::window().unwrap().location().search().unwrap();
-            let query_level: Option<log::LevelFilter> = parse_url_query_string(&query_string, "RUST_LOG")
-                .and_then(|x| x.parse().ok());
-
-            // 我们将 wgpu 日志级别保持在错误级别，因为 Info 级别的日志输出非常多。
-            let base_level = query_level.unwrap_or(log::LevelFilter::Info);
-            let wgpu_level = query_level.unwrap_or(log::LevelFilter::Error);
-
-            // 在 web 上，我们使用 fern，因为 console_log 没有按模块级别过滤功能。
-            fern::Dispatch::new()
-                .level(base_level)
-                .level_for("wgpu_core", wgpu_level)
-                .level_for("wgpu_hal", wgpu_level)
-                .level_for("naga", wgpu_level)
-                .chain(fern::Output::call(console_log::log))
-                .apply()
-                .unwrap();
-            std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-        } else if #[cfg(target_os = "android")] {
-            // 添加 Android 平台的日志初始化
-            android_logger::init_once(
-                android_logger::Config::default()
-                    .with_max_level(log::LevelFilter::Info)
-            );
-            log_panics::init();
-        } else {
-            // parse_default_env 会读取 RUST_LOG 环境变量，并在这些默认过滤器之上应用它。
-            env_logger::builder()
-                .filter_level(log::LevelFilter::Info)
-                .filter_module("wgpu_core", log::LevelFilter::Info)
-                .filter_module("wgpu_hal", log::LevelFilter::Error)
-                .filter_module("naga", log::LevelFilter::Error)
-                .parse_default_env()
-                .init();
-        }
-    }
-}
-
 fn main() -> Result<(), impl std::error::Error> {
-    init_logger();
+    wgpu_dance::init_logger();
 
     let events_loop = EventLoop::new().unwrap();
     let mut app = WgpuAppHandler::default();
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,42 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+// 根据不同平台初始化日志。
+pub fn init_logger() {
+    cfg_if::cfg_if! {
+        if #[cfg(target_arch = "wasm32")] {
+            // 使用查询字符串来获取日志级别。
+            let query_string = web_sys::window().unwrap().location().search().unwrap();
+            let query_level: Option<log::LevelFilter> = parse_url_query_string(&query_string, "RUST_LOG")
+                .and_then(|x| x.parse().ok());
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+            // 我们将 wgpu 日志级别保持在错误级别，因为 Info 级别的日志输出非常多。
+            let base_level = query_level.unwrap_or(log::LevelFilter::Info);
+            let wgpu_level = query_level.unwrap_or(log::LevelFilter::Error);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+            // 在 web 上，我们使用 fern，因为 console_log 没有按模块级别过滤功能。
+            fern::Dispatch::new()
+                .level(base_level)
+                .level_for("wgpu_core", wgpu_level)
+                .level_for("wgpu_hal", wgpu_level)
+                .level_for("naga", wgpu_level)
+                .chain(fern::Output::call(console_log::log))
+                .apply()
+                .unwrap();
+            std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+        } else if #[cfg(target_os = "android")] {
+            // 添加 Android 平台的日志初始化
+            android_logger::init_once(
+                android_logger::Config::default()
+                    .with_max_level(log::LevelFilter::Info)
+            );
+            log_panics::init();
+        } else {
+            // parse_default_env 会读取 RUST_LOG 环境变量，并在这些默认过滤器之上应用它。
+            env_logger::builder()
+                .filter_level(log::LevelFilter::Info)
+                .filter_module("wgpu_core", log::LevelFilter::Info)
+                .filter_module("wgpu_hal", log::LevelFilter::Error)
+                .filter_module("naga", log::LevelFilter::Error)
+                .parse_default_env()
+                .init();
+        }
     }
 }
