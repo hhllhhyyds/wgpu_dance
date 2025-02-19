2ce2044d3c18e8947eb359806ca5cf63c24196c5
hhllhhyyds <hhllhhyyds@sina.com>
2025-02-19 09:43:25 +0800

    new version wgpu, create window

--- src/bin/triangle.rs
+++ src/bin/triangle.rs
@@ -1,232 +1,126 @@
+use std::{rc::Rc, sync::Arc};
+
+use parking_lot::Mutex;
 use winit::{
-    event::*,
-    event_loop::{ControlFlow, EventLoop},
-    window::{Window, WindowBuilder},
+    application::ApplicationHandler,
+    event::{KeyEvent, WindowEvent},
+    event_loop::{ActiveEventLoop, EventLoop},
+    keyboard::{KeyCode, PhysicalKey},
+    window::{Window, WindowId},
 };
 
-struct State {
-    surface: wgpu::Surface,
-    device: wgpu::Device,
-    queue: wgpu::Queue,
-    config: wgpu::SurfaceConfiguration,
-    size: winit::dpi::PhysicalSize<u32>,
-    background_color: wgpu::Color,
-    render_pipeline: wgpu::RenderPipeline,
+struct WgpuApp {
+    /// 避免窗口被释放
+    #[allow(unused)]
+    window: Arc<Window>,
 }
 
-impl State {
-    // 某些 wgpu 类型需要使用异步代码才能创建
-    async fn new(window: &Window) -> Self {
-        let size = window.inner_size();
-
-        // instance 变量是到 GPU 的 handle
-        // Backends::all 对应 Vulkan + Metal + DX12 + 浏览器的 WebGPU
-        let instance = wgpu::Instance::new(wgpu::Backends::all());
-        let surface = unsafe { instance.create_surface(window) };
-        let adapter = instance
-            .request_adapter(&wgpu::RequestAdapterOptions {
-                power_preference: wgpu::PowerPreference::default(),
-                compatible_surface: Some(&surface),
-                force_fallback_adapter: false,
-            })
-            .await
-            .unwrap();
-
-        let (device, queue) = adapter
-            .request_device(
-                &wgpu::DeviceDescriptor {
-                    features: wgpu::Features::empty(),
-                    limits: wgpu::Limits::default(),
-                    label: None,
-                },
-                None, // 是否追踪 API 调用路径
-            )
-            .await
-            .unwrap();
-
-        let config = wgpu::SurfaceConfiguration {
-            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-            format: surface.get_preferred_format(&adapter).unwrap(),
-            width: size.width,
-            height: size.height,
-            present_mode: wgpu::PresentMode::Fifo,
-        };
-        surface.configure(&device, &config);
-
-        let shader = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
-            label: Some("Shader"),
-            source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
-        });
-
-        let render_pipeline_layout =
-            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-                label: Some("Render Pipeline Layout"),
-                bind_group_layouts: &[],
-                push_constant_ranges: &[],
-            });
-
-        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-            label: Some("Render Pipeline"),
-            layout: Some(&render_pipeline_layout),
-            vertex: wgpu::VertexState {
-                module: &shader,
-                entry_point: "vs_main", // 1.
-                buffers: &[],           // 2.
-            },
-            fragment: Some(wgpu::FragmentState {
-                // 3.
-                module: &shader,
-                entry_point: "fs_main",
-                targets: &[wgpu::ColorTargetState {
-                    // 4.
-                    format: config.format,
-                    blend: Some(wgpu::BlendState::REPLACE),
-                    write_mask: wgpu::ColorWrites::ALL,
-                }],
-            }),
-            primitive: wgpu::PrimitiveState {
-                topology: wgpu::PrimitiveTopology::TriangleList, // 1.
-                strip_index_format: None,
-                front_face: wgpu::FrontFace::Ccw, // 2.
-                cull_mode: Some(wgpu::Face::Back),
-                // 如果将该字段设置为除了 Fill 之外的任何值，都需要 Features::NON_FILL_POLYGON_MODE
-                polygon_mode: wgpu::PolygonMode::Fill,
-                // 需要 Features::DEPTH_CLIP_ENABLE
-                unclipped_depth: false,
-                // 需要 Features::CONSERVATIVE_RASTERIZATION
-                conservative: false,
-            },
-            depth_stencil: None, // 1.
-            multisample: wgpu::MultisampleState {
-                count: 1,                         // 2.
-                mask: !0,                         // 3.
-                alpha_to_coverage_enabled: false, // 4.
-            },
-            multiview: None, // 5.
-        });
-
-        Self {
-            surface,
-            device,
-            queue,
-            config,
-            size,
-            background_color: wgpu::Color {
-                r: 0.1,
-                g: 0.2,
-                b: 0.3,
-                a: 1.,
-            },
-            render_pipeline,
-        }
-    }
-
-    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
-        if new_size.width > 0 && new_size.height > 0 {
-            self.size = new_size;
-            self.config.width = new_size.width;
-            self.config.height = new_size.height;
-            self.surface.configure(&self.device, &self.config);
-        }
-    }
-
-    fn input(&mut self, event: &WindowEvent) -> bool {
-        false
-    }
-
-    fn update(&mut self) {
-        // todo!()
-    }
-
-    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
-        let output = self.surface.get_current_texture()?;
-        let view = output
-            .texture
-            .create_view(&wgpu::TextureViewDescriptor::default());
-        let mut encoder = self
-            .device
-            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
-                label: Some("Render Encoder"),
-            });
-
-        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-            label: Some("Render Pass"),
-            color_attachments: &[wgpu::RenderPassColorAttachment {
-                view: &view,
-                resolve_target: None,
-                ops: wgpu::Operations {
-                    load: wgpu::LoadOp::Clear(self.background_color),
-                    store: true,
-                },
-            }],
-            depth_stencil_attachment: None,
-        });
-        render_pass.set_pipeline(&self.render_pipeline);
-        render_pass.draw(0..3, 0..1);
-        drop(render_pass);
-
-        // submit 方法能传入任何实现了 IntoIter 的参数
-        self.queue.submit(std::iter::once(encoder.finish()));
-        output.present();
-
-        Ok(())
+impl WgpuApp {
+    async fn new(window: Arc<Window>) -> Self {
+        // ...
+        Self { window }
     }
 }
 
-fn main() {
-    env_logger::init();
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
+#[derive(Default)]
+struct WgpuAppHandler {
+    app: Rc<Mutex<Option<WgpuApp>>>,
+}
 
-    let mut state = pollster::block_on(State::new(&window));
+impl ApplicationHandler for WgpuAppHandler {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        // 恢复事件
+        if self.app.as_ref().lock().is_some() {
+            return;
+        }
 
-    event_loop.run(move |event, _, control_flow| match event {
-        Event::WindowEvent {
-            ref event,
-            window_id,
-        } if window_id == window.id() => match event {
-            WindowEvent::CloseRequested
-            | WindowEvent::KeyboardInput {
-                input:
-                    KeyboardInput {
-                        state: ElementState::Pressed,
-                        virtual_keycode: Some(VirtualKeyCode::Escape),
-                        ..
-                    },
+        let window_attributes = Window::default_attributes().with_title("tutorial1-window");
+        let window = Arc::new(event_loop.create_window(window_attributes).unwrap());
+
+        let wgpu_app = pollster::block_on(WgpuApp::new(window));
+        self.app.lock().replace(wgpu_app);
+    }
+
+    fn suspended(&mut self, _event_loop: &ActiveEventLoop) {
+        // 暂停事件
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        _window_id: WindowId,
+        window_event: WindowEvent,
+    ) {
+        // 窗口事件
+        match window_event {
+            WindowEvent::CloseRequested => {
+                event_loop.exit();
+            }
+            WindowEvent::Resized(_size) => {
+                // 窗口大小改变
+            }
+            WindowEvent::KeyboardInput {
+                event: KeyEvent { physical_key, .. },
                 ..
-            } => *control_flow = ControlFlow::Exit,
-            WindowEvent::Resized(physical_size) => {
-                state.resize(*physical_size);
+            } => match physical_key {
+                PhysicalKey::Code(KeyCode::Escape) => event_loop.exit(),
+                _ => {}
+            },
+            WindowEvent::RedrawRequested => {
+                // surface重绘事件
             }
-            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                // new_inner_size 是 &&mut 类型，因此需要解引用两次
-                state.resize(**new_inner_size);
-            }
-            WindowEvent::CursorMoved { .. } => {
-                state.background_color.r += 0.1;
-                if state.background_color.r > 1. {
-                    state.background_color.r = 0.
-                }
-            }
+            _ => (),
+        }
+    }
+}
 
-            _ => {}
-        },
-        Event::RedrawRequested(window_id) if window_id == window.id() => {
-            state.update();
-            match state.render() {
-                Ok(_) => {}
-                // 如果发生上下文丢失，就重新配置 surface
-                Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
-                // 系统内存不足，此时应该退出
-                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
-                // 所有其他错误（如过时、超时等）都应在下一帧解决
-                Err(e) => eprintln!("{:?}", e),
-            }
+// 根据不同平台初始化日志。
+pub fn init_logger() {
+    cfg_if::cfg_if! {
+        if #[cfg(target_arch = "wasm32")] {
+            // 使用查询字符串来获取日志级别。
+            let query_string = web_sys::window().unwrap().location().search().unwrap();
+            let query_level: Option<log::LevelFilter> = parse_url_query_string(&query_string, "RUST_LOG")
+                .and_then(|x| x.parse().ok());
+
+            // 我们将 wgpu 日志级别保持在错误级别，因为 Info 级别的日志输出非常多。
+            let base_level = query_level.unwrap_or(log::LevelFilter::Info);
+            let wgpu_level = query_level.unwrap_or(log::LevelFilter::Error);
+
+            // 在 web 上，我们使用 fern，因为 console_log 没有按模块级别过滤功能。
+            fern::Dispatch::new()
+                .level(base_level)
+                .level_for("wgpu_core", wgpu_level)
+                .level_for("wgpu_hal", wgpu_level)
+                .level_for("naga", wgpu_level)
+                .chain(fern::Output::call(console_log::log))
+                .apply()
+                .unwrap();
+            std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+        } else if #[cfg(target_os = "android")] {
+            // 添加 Android 平台的日志初始化
+            android_logger::init_once(
+                android_logger::Config::default()
+                    .with_max_level(log::LevelFilter::Info)
+            );
+            log_panics::init();
+        } else {
+            // parse_default_env 会读取 RUST_LOG 环境变量，并在这些默认过滤器之上应用它。
+            env_logger::builder()
+                .filter_level(log::LevelFilter::Info)
+                .filter_module("wgpu_core", log::LevelFilter::Info)
+                .filter_module("wgpu_hal", log::LevelFilter::Error)
+                .filter_module("naga", log::LevelFilter::Error)
+                .parse_default_env()
+                .init();
         }
-        Event::MainEventsCleared => {
-            // 除非手动请求，否则 RedrawRequested 只会触发一次
-            window.request_redraw();
-        }
-        _ => {}
-    });
+    }
+}
+
+fn main() -> Result<(), impl std::error::Error> {
+    init_logger();
+
+    let events_loop = EventLoop::new().unwrap();
+    let mut app = WgpuAppHandler::default();
+    events_loop.run_app(&mut app)
 }
